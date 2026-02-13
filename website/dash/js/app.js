b99d7c391efe579730b33943f39a1498d91f8581
kornelhaha <kornelhahalmfao@gmail.com>
2026-02-13 17:01:36 +0100

    Update app.js

--- website/dash/js/app.js
+++ website/dash/js/app.js
@@ -1,12 +1,12 @@
 window.API_URL = 'https://asce-f5us.onrender.com';
 
-let currentUser = null;
-let currentConfig = {};
-let pendingUpdates = {};
-let lastSentConfig = {};
-let saveTimeout = null;
-let isUpdatingFromServer = false;
-const SAVE_DEBOUNCE_MS = 500;
+window.currentUser = null;
+window.currentConfig = {};
+window.pendingUpdates = {};
+window.lastSentConfig = {};
+window.saveTimeout = null;
+window.isUpdatingFromServer = false;
+window.SAVE_DEBOUNCE_MS = 500;
 
 async function init() {
     try {
